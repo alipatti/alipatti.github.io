e2b8d9c6611d99575c199ae9902db5f7c3ffc124
Alistair Pattison <alistairpattison@gmail.com>
2023-06-23 17:32:31 -0500

    resume and bio updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,27 @@
 import { FaFile, FaGithub, FaLinkedin } from "react-icons/fa";
 import Image from "next/image";
-import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="container mx-auto grid justify-center gap-10 p-4 md:max-w-2xl lg:max-w-5xl lg:p-10">
+    <div className="container mx-auto grid justify-center gap-10 p-4 md:max-w-2xl lg:p-10">
       <h1 className="text-6xl">Alistair Pattison</h1>
 
-      <section className="flex flex-col items-center gap-10 lg:flex-row">
+      <section className="flex flex-col items-center gap-10">
         <Image
           className="aspect-square rounded-full object-cover object-left-top"
           src={"/photos/me.jpeg"}
-          alt="Dashing gentleman man overlooks valley in the Austrian alps."
+          alt="Me in Austria."
           height={280}
           width={280}
         ></Image>
         <div>
           <p id="about" className="">
-            I'm Alistair, a student at Carleton College studying math and
-            statistics. Recently, I've worked as a research assistant in the
-            Privacy and Cryptography lab at the University of Minnesota and as a
-            teaching assistant for the math and computer science departments at
-            Carleton. My{" "}
+            I'm Alistair, a undergrad at Carleton College studying math,
+            statistics, and computer science. Recently, I've worked as a
+            researcher in the Privacy and Cryptography lab at the University of
+            Minnesota, where my work has focused on using tools from cryptography to
+            develop ways of moderateing encrypted messaging platforms while
+            respecting users' anonymity and privacy. My{" "}
             <a href="https://link.springer.com/article/10.1007/BF02187731">
               Erdös
             </a>{" "}
@@ -31,24 +31,22 @@ export default function Home() {
             <a href="https://link.springer.com/chapter/10.1007/3-540-39200-9_18">
               is
             </a>{" "}
-            <a href="TODO: add arXiv link">four</a>, and my other academic
-            interests include artificial intelligence, evolutionary biology, and
-            physics. Outside of school, I am an{" "}
+            <a href="https://arxiv.org/abs/2306.01241">four.</a>
+          </p>
+
+          <p>
+            Outside of school, I am a captain of Carleton's{" "}
             <a
               href="https://athletics.carleton.edu/news/2023/2/11/mens-track-and-field-knights-win-7-times-at-the-meet-of-the-hearts.aspx"
               target="_blank"
             >
-              avid runner
+              track and field team
             </a>
             {", "}
             <a href="https://github.com/alipatti" target="_blank">
-              coder
+              an avid coder
             </a>
-            {", "}
-            <Link href="/photos/wilmore.jpg" target="_blank">
-              nature enthusiast
-            </Link>
-            {", "}
+            {", "}nature enthusiast,{" "}
             <a href="https://github.com/alipatti/dotfiles" target="_blank">
               chronic bikeshedder
             </a>
@@ -79,4 +77,4 @@ export default function Home() {
   );
 }
 
-const getRepos = () => {};
+const getRepos = () => { };

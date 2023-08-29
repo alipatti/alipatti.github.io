3c0d0df8cfcaefabd2ff1f610abf54c5d6f6f7f7
Alistair Pattison <alistairpattison@gmail.com>
2023-08-29 15:03:43 -0500

    august 2023 update

--- app/page.tsx
+++ app/page.tsx
@@ -6,21 +6,44 @@ export default function Home() {
     <div className="container mx-auto grid justify-center gap-10 p-4 md:max-w-2xl lg:p-10">
       <h1 className="text-6xl">Alistair Pattison</h1>
 
-      <section className="flex flex-col items-center gap-10">
-        <Image
-          className="aspect-square rounded-full object-cover object-left-top"
-          src={"/photos/me.jpeg"}
-          alt="Me in Austria."
-          height={280}
-          width={280}
-        ></Image>
-        <div>
-          <p id="about" className="text-center">
+      <main className="flex flex-col items-center gap-10">
+        {/* Headshot image */}
+        <section id="headshot">
+          <Image
+            className="aspect-square rounded-full object-cover object-left-top"
+            src={"/photos/me.jpeg"}
+            alt="Me in Austria."
+            height={280}
+            width={280}
+          ></Image>
+        </section>
+
+        {/* Links */}
+        <section
+          id="links"
+          className="px-auto children:rounded children:bg-[#7C4C5C] children:px-3 children:py-2 flex flex-wrap items-center justify-center gap-10 text-gray-200"
+        >
+          <a className="hover:shadow" href="https://github.com/alipatti">
+            <FaGithub className="inline" /> <span className="pl-2">github</span>
+          </a>
+          <a className="hover:shadow " href="https://linked.com/in/alipatti">
+            <FaLinkedin className="inline" />{" "}
+            <span className="pl-2">linkedin</span>
+          </a>
+          <a className="hover:shadow " href="/resume.pdf">
+            <FaFile className="inline" /> <span className="pl-2">cv</span>
+          </a>
+        </section>
+
+        {/* Bio paragraph */}
+        <section id="bio">
+          <p className="text-center">
             I'm Alistair, a undergrad at Carleton College studying math,
             statistics, and computer science. Recently, I've worked as a
             researcher at the University of Minnesota, using tools from
             cryptography to develop a privacy-preserving method for reporting
-            abuse and misinformation on encrypted messaging platforms. My{" "}
+            abuse and misinformation on encrypted messaging platforms like
+            WhatsApp and Signal. My{" "}
             <a href="https://link.springer.com/article/10.1007/BF02187731">
               Erdös
             </a>{" "}
@@ -64,24 +87,8 @@ export default function Home() {
             </li>
             <li>pickup sports of any kind</li>
           </ul>
-        </div>
-      </section>
-
-      <section
-        id="links"
-        className="px-auto children:rounded children:bg-[#7C4C5C] children:px-3 children:py-2 flex flex-wrap items-center justify-center gap-10 text-gray-200"
-      >
-        <a className="hover:shadow" href="https://github.com/alipatti">
-          <FaGithub className="inline" /> <span className="pl-2">github</span>
-        </a>
-        <a className="hover:shadow " href="https://linked.com/in/alipatti">
-          <FaLinkedin className="inline" />{" "}
-          <span className="pl-2">linkedin</span>
-        </a>
-        <a className="hover:shadow " href="/resume.pdf">
-          <FaFile className="inline" /> <span className="pl-2">cv</span>
-        </a>
-      </section>
+        </section>
+      </main>
 
       <section id="github"></section>
     </div>

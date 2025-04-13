e15ed32c77cb2cb739aa4a0de496001f70b2bba6
Alistair Pattison <alistairpattison@gmail.com>
2025-04-12 20:03:24 -0400

    replicate next website

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,55 @@
 <script lang="ts">
-	import '../app.css';
-	
-	let { children } = $props();
+  import "../app.css";
+
+  let { children } = $props();
 </script>
 
-{@render children()}
+<div class="min-h-screen flex flex-col">
+  <nav class="bg-white/50 px-6 py-3 text-lg absolute w-full">
+    <ul class="flex items-baseline gap-10">
+      <a href="/" class="text-red-500">
+        <li class="font-mono">alipatti.com</li>
+      </a>
+    </ul>
+  </nav>
+
+  <div
+    class="grow text-neutral-800 dark:bg-neutral-800 dark:text-neutral-100"
+  >
+    {@render children()}
+  </div>
+</div>
+
+<footer
+  class="bg-darkgreen/70 dark:text-lightblue/50 z-50 p-5 text-center text-sm font-light text-white/60"
+>
+  <div
+    class="descendant-span:ml-2 mx-auto descendant-a:hover:underline mb-3 flex w-1/2 flex-wrap justify-center gap-x-3 gap-y-1"
+  >
+    <a href="https://linkedin.com/in/alipatti">
+      <span class="fa--linkedin"></span>
+      <span>linkedin.com/in/alipatti</span>
+    </a>
+
+    <a href="https://github.com/alipatti">
+      <span class="fa--github"></span>
+      <span>github.com/alipatti</span>
+    </a>
+
+    <a href="https://instagram.com/a1ipatti">
+      <span class="fa--instagram"></span>
+      <span>instagram.com/a1ipatti</span>
+    </a>
+  </div>
+
+  <hr class="mx-auto mb-3 w-10 opacity-10 dark:opacity-5" />
+
+  <span class="mb-3 block">
+    <a
+      href="https://github.com/alipatti/alipatti.github.io"
+      class="hover:underline"
+    >
+      website source
+    </a>
+  </span>
+</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,122 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import headshot from "$lib/assets/headshot.jpeg";
+  import { isMandelbrot } from "$lib/mandelbrot.ts";
+  import { innerWidth } from "svelte/reactivity/window";
+
+  const maxIters = 100;
+
+  let canvas: HTMLCanvasElement;
+
+  const center = [0.2, -1]; // real, im
+  const zoom = 2000;
+
+  $effect(() => {
+    innerWidth.current; // reference so effect re-runs when window resizes
+
+    canvas.width = canvas.offsetWidth;
+    canvas.height = canvas.offsetHeight;
+
+    const w = canvas.width;
+    const h = canvas.height;
+
+    const context = canvas.getContext("2d")!;
+    const imageData = context.createImageData(w, h);
+
+    for (let x = 0; x < w; x += 1) {
+      for (let y = 0; y < h; y += 1) {
+        let iters = isMandelbrot(
+          center[0] + (x - w / 2) / zoom,
+          -center[1] + (y - h / 2) / zoom,
+          maxIters,
+        );
+
+        const i = (x + y * w) * 4;
+        imageData.data[i + 3] = iters == maxIters ? 20 : iters * 4; // alpha
+      }
+    }
+
+    context.putImageData(imageData, 0, 0);
+  });
+</script>
+
+<main class="min-h-screen relative">
+  <canvas
+    class="absolute blur-[2px] dark:invert-75 w-full h-full"
+    bind:this={canvas}
+  ></canvas>
+
+  <section id="bio" class="min-h-screen">
+    <div
+      class="mx-auto flex flex-col items-center justify-start gap-10 text-center xl:min-w-[40rem] xl:max-w-6xl xl:gap-5 2xl:pt-40 xl:pt-20 p-10"
+    >
+      <h1 class="text-6xl">Alistair Pattison</h1>
+
+      <hr
+        class="mx-auto block h-px w-60 border-0 bg-neutral-700 opacity-50 dark:bg-neutral-500"
+      />
+
+      <div class="flex flex-col items-center">
+        <div class="flex flex-col items-center gap-20 2xl:flex-row">
+          <img
+            class="aspect-square rounded-full object-cover object-left-top shadow-md"
+            src={headshot}
+            alt="headshot"
+            height={280}
+            width={280}
+          />
+
+          <div class="max-w-md space-y-5 text-center">
+            <p>
+              I'm a predoctoral fellow for <a
+                href="https://opportunityinsights.org">Opportunity Insights</a
+              >, a research group at Harvard that uses big data to identify
+              barriers to social mobility in the U.S. with the goal of reviving
+              the American dream. Previously, I studied math and statistics at
+              <a
+                target="_blank"
+                href="https://www.startribune.com/minnesota-winter-weather-temperature-cold-north-pole/600114153/"
+              >
+                Carleton College
+              </a>
+              in Minnesota where I also spent a lot of time
+              <a
+                target="_blank"
+                href="https://athletics.carleton.edu/news/2024/5/10/mens-track-and-field-2024-miac-outdoor-championships-day-1.aspx"
+              >
+                running in circles
+              </a>.
+            </p>
+            <p>
+              If for some reason you'd like to learn more about me, you can find
+              more at the links below.
+            </p>
+          </div>
+        </div>
+
+        <div
+          id="links"
+          class="px-auto mt-10 flex flex-wrap items-center justify-center gap-5 sm:gap-10"
+        >
+          <a class="button hover:shadow" href="https://github.com/alipatti">
+            <span class="fa--github"></span>
+            <span class="pl-2">github</span>
+          </a>
+          <a
+            class="button hover:shadow"
+            href="https://linkedin.com/in/alipatti"
+          >
+            <span class="fa--linkedin-square"></span>
+            <span class="pl-2">linkedin</span>
+          </a>
+          <a
+            class="button hover:shadow"
+            href="https://github.com/alipatti/cv/releases/latest/download/pattison-cv-full.pdf"
+          >
+            <span class="fa-solid--file-pdf"></span>
+            <span class="pl-2">cv</span>
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+</main>

f987f8991eece32e0a381de1ef24c5c9e47f6b29
Alistair Pattison <alistairpattison@gmail.com>
2025-09-20 18:51:54 -0400

    update header/footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,10 +7,20 @@
   class="min-h-screen flex flex-col grow text-neutral-800 dark:bg-neutral-800 dark:text-neutral-100 relative"
 >
   <nav class="bg-white/50 px-6 py-3 text-lg w-full">
-    <ul class="flex items-baseline gap-10">
-      <a href="/" class="text-red-500">
-        <li class="font-mono">alipatti.com</li>
-      </a>
+    <ul class="flex items-baseline gap-10 justify-between">
+      <div>
+        <a href="/" class="text-red-500">
+          <li class="font-mono">alipatti.com</li>
+        </a>
+      </div>
+      <div class="space-x-4 text-xl">
+        <a href="https://linkedin.com/in/alipatti">
+          <span class="fa--linkedin-square text-neutral-300 hover:text-blue"></span>
+        </a>
+        <a href="https://github.com/alipatti">
+          <span class="fa--github text-neutral-300 hover:text-blue"></span>
+        </a>
+      </div>
     </ul>
   </nav>
 
@@ -24,7 +34,7 @@
     class="descendant-span:ml-2 mx-auto descendant-a:hover:underline mb-3 flex w-1/2 flex-wrap justify-center gap-x-3 gap-y-1"
   >
     <a href="https://linkedin.com/in/alipatti">
-      <span class="fa--linkedin"></span>
+      <span class="fa--linkedin-square"></span>
       <span>linkedin.com/in/alipatti</span>
     </a>
 
@@ -33,15 +43,4 @@
       <span>github.com/alipatti</span>
     </a>
   </div>
-
-  <hr class="mx-auto mb-3 w-10 opacity-10 dark:opacity-5" />
-
-  <span class="mb-3 block">
-    <a
-      href="https://github.com/alipatti/alipatti.github.io"
-      class="hover:underline"
-    >
-      website source
-    </a>
-  </span>
 </footer>

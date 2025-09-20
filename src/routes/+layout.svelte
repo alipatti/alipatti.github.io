4f9a20f5e3e0339146b42c3ab5b461b434a6d640
Alistair Pattison <alistairpattison@gmail.com>
2025-09-20 18:11:41 -0400

    merge update to svelte

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+  import "../app.css";
+  let { children } = $props();
+</script>
+
+<div
+  class="min-h-screen flex flex-col grow text-neutral-800 dark:bg-neutral-800 dark:text-neutral-100 relative"
+>
+  <nav class="bg-white/50 px-6 py-3 text-lg w-full">
+    <ul class="flex items-baseline gap-10">
+      <a href="/" class="text-red-500">
+        <li class="font-mono">alipatti.com</li>
+      </a>
+    </ul>
+  </nav>
+
+  {@render children()}
+</div>
+
+<footer
+  class="bg-darkgreen/70 dark:text-lightblue/50 z-50 p-5 text-center text-sm font-light text-white/60"
+>
+  <div
+    class="descendant-span:ml-2 mx-auto descendant-a:hover:underline mb-3 flex w-1/2 flex-wrap justify-center gap-x-3 gap-y-1"
+  >
+    <a href="https://linkedin.com/in/alipatti">
+      <span class="fa--linkedin"></span>
+      <span>linkedin.com/in/alipatti</span>
+    </a>
+
+    <a href="https://github.com/alipatti">
+      <span class="fa--github"></span>
+      <span>github.com/alipatti</span>
+    </a>
+  </div>
+
+  <hr class="mx-auto mb-3 w-10 opacity-10 dark:opacity-5" />
+
+  <span class="mb-3 block">
+    <a
+      href="https://github.com/alipatti/alipatti.github.io"
+      class="hover:underline"
+    >
+      website source
+    </a>
+  </span>
+</footer>

4fa262c85c65adae5ec2a9c01d8bffaa5c704b9f
Alistair Pattison <alistairpattison@gmail.com>
2026-02-02 17:11:41 -0500

    personal page?

---
+++ src/routes/personal/+page.svelte
@@ -0,0 +1,35 @@
+<main class="p-5 sm:p-20 max-w-5xl mx-auto lg:w-4xl">
+  <h1>Personal</h1>
+
+  <p>
+    Things I like:
+    <!-- cone lamp -->
+    <a href="/cone-lamp-1.jpg">interior</a>
+    <a href="/cone-lamp-2.jpg">design</a>,
+    <!-- outside -->
+    <a href="/outside-1.jpg">being</a>
+    <a href="/outside-2.jpg">out</a><a href="/outside-3.jpg">side</a>,
+    <!-- sports -->
+    <a href="/wheel-on-backwards.jpg">biking</a>,
+    <!-- misc -->
+    <a href="./cow-2.jpg">cows</a>,
+    <!-- articles -->
+    <a
+      href="https://www.theatlantic.com/magazine/archive/2021/04/private-schools-are-indefensible/618078/?gift=S-oeiBnHh4soHDk2SEfXfePQV6U_sCP2-Ww21Q4ATag&utm_source=copy-link&utm_medium=social&utm_campaign=share"
+      >public schools</a
+    >,
+    <a
+      href="https://www.nytimes.com/2025/05/19/style/ai-slop-slop-bowls-shein-slop-hauls.html"
+      >slop bowls</a
+    >.
+  </p>
+
+  <p>
+    I grew up in Saint Paul, Minnesota. My parents are <a
+      href="https://www.mprnews.org/people/maja-beckstrom-mprnews">both</a
+    >
+    <a href="https://www.goodreads.com/book/show/30653582-fossil-men"
+      >journalists</a
+    >. My older brother works for the NBA. My younger sister is in college.
+  </p>
+</main>

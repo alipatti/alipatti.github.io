56bd876d7fca542a48d3f3fad8758407a82d60b2
Alistair Pattison <alistairpattison@gmail.com>
2026-05-08 16:21:45 -0400

    Revert "redirect /cv"
    This reverts commit 28acef9f888b35266b698a65cf8aed6e836c698f.

--- src/routes/cv/+page.svelte
+++
@@ -1,8 +0,0 @@
-<svelte:head>
-  <meta http-equiv="refresh" content="0; url=/cv/pattison-cv.pdf" />
-</svelte:head>
-
-<script>
-  import { browser } from "$app/environment";
-  if (browser) window.location.replace("/cv/pattison-cv.pdf");
-</script>

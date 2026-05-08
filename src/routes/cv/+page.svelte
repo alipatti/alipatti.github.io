28acef9f888b35266b698a65cf8aed6e836c698f
Alistair Pattison <alistairpattison@gmail.com>
2026-05-08 16:17:53 -0400

    redirect /cv

---
+++ src/routes/cv/+page.svelte
@@ -0,0 +1,8 @@
+<svelte:head>
+  <meta http-equiv="refresh" content="0; url=/cv/pattison-cv.pdf" />
+</svelte:head>
+
+<script>
+  import { browser } from "$app/environment";
+  if (browser) window.location.replace("/cv/pattison-cv.pdf");
+</script>
